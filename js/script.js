ba18544c3475374b403f284a2b327c462691922b
Sergiy Dudyuk <s.dudyuk.o@gmail.com>
2020-05-23 20:59:21 +0300

    Question 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 const numberOfFilms = +prompt("Скільки фільмів Ви переглянули?", "0");
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
